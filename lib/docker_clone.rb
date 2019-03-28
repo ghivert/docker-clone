d7b765290677fcaf734751318ee3b7707d8710af
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-03-28 14:33:15 +0100

    :recycle: Move CLI options parser into binary

--- lib/docker_clone.rb
+++ lib/docker_clone.rb
@@ -2,25 +2,22 @@ require "yaml"
 require "optparse"
 require "pathname"
 
-CONFIG_DESCRIPTION  = "Path to your docker clone config file"
-HELP_DESCRIPTION    = "Show this help message"
-
-HELP_BANNER = "Usage: docker-clone [options]"
-
 DEFAULT_WORKING_DIR = "../"
 
 class NoRepoException < Exception
 end
 
 class DockerClone
-  def initialize
+  def initialize(options = {})
+    raise ArgumentError.new("No Config File Path Given.") if !options[:config_file_path]
+    @config_file_path = full_path(options[:config_file_path])
+    @working_dir = options[:working_dir] || DEFAULT_WORKING_DIR
     get_config
   end
 
   def clone_repos
-    working_dir = @@options[:working_dir] || DEFAULT_WORKING_DIR
-    Dir.chdir(working_dir) do
-      @@docker_clone["repos"].each do |repo|
+    Dir.chdir(@working_dir) do
+      @docker_clone_config["repos"].each do |repo|
         clone_repo(repo)
       end
     end
@@ -28,56 +25,34 @@ class DockerClone
 
   private
 
-  def parse_options
-    options = { config_path: "docker-clone.yml" }
-    options.tap do |options|
-      OptionParser.new do |parser|
-        parser.banner = HELP_BANNER
-        parser.separator ""
-        parser.on("-h", "--help", HELP_DESCRIPTION) do
-          puts parser
-          exit(true)
-        end
-        parser.on("-c", "--config config_file_path", CONFIG_DESCRIPTION) do |config_path|
-          options[:config_path] = config_path
-        end
-      end.parse!
-    end
-  end
-
   def full_path(pathname)
     path = Pathname.new(pathname)
     path.expand_path
   end
 
-  def read_docker_file(pathname)
-    path = full_path(pathname)
-    file = File.read(path)
+  def read_docker_file
+    file = File.read(@config_file_path)
     YAML.load(file)
   end
 
   def print_docker_clone_error(path)
-    puts "No corresponding config file, looking for #{path}"
+    raise ArgumentError.new("No corresponding config file, looking for #{path}")
   end
 
   def print_docker_clone_error_and_exit
-    config_path      = @@options[:config_path]
-    full_config_path = full_path(config_path)
-
-    print_docker_clone_error(full_config_path)
+    print_docker_clone_error(@config_file_path)
     exit(false)
   end
 
   def get_config
-    @@options      ||= parse_options
-    @@docker_clone ||= read_docker_file(@@options[:config_path])
+    @docker_clone_config ||= read_docker_file
   rescue
     print_docker_clone_error_and_exit
   end
 
   def clone_repo(repo)
     url, name = repo["url"], repo["name"]
-    puts "No URL or name given for your repo. #{url} #{name}" if !url || !name
+    raise ArgumentError.new("No URL or name given for your repo. #{url} #{name}") if !url || !name
     `git clone #{url} #{name}`
   end
 end
